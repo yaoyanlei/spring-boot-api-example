2bed94588889d44fef1ee2019af53ea2a9ac8a9d
yanlei.yao <yanlei.yao@dianrong.com>
2020-05-16 14:14:58 +0800

    Initial commit

---
+++ src/main/java/com/apelab/example/SpringBootApiExampleApplication.java
@@ -0,0 +1,12 @@
+package com.apelab.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootApiExampleApplication.class, args);
+  }
+}
---
+++ src/test/java/com/apelab/example/SpringBootApiExampleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.apelab.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
